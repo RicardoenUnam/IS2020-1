18294cd054cf83c6c13ca7d323c6c779d8621d2b
Ricardo Ruiz <ori@ciencias.unam.mx>
2019-08-13 22:57:57 -0500

    final

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 ### 12 Agosto 2019
+<<<<<<< HEAD
 #### 3 archivos, 3 clases, python
+=======
+##### Práctica 01
+##### Python and Git
+>>>>>>> changes
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from alumno import *
 from materia import *
 from universidad import *
 
-p1 = Alumno(input("Escribe tu nombre: "), input("Escribe tu numero de cuenta: "))
+p1 = Alumno(input("Escribe tu nombre: "), input("Escribe tu # de cuenta: "))
 
 p2 = Materia(input("Escribe la materia que quieras registrar: "), input("Escribe el codigo de la materia: "))
 
 p3 = Universidad(input("Escribe el nombre de la universidad: "), input("Escribe el nombre del pais: "))
 
 print(p1.name + "(" + str(p1.cuenta) + "), la materia: " + str(p2.name) +" con el codigo " + str(p2.codigo) + " ha quedado registrada.")
-print("Bienvenido a la " + str(p3.name)  + " en " + str(p3.pais))
+print("Bienvenido a la " + str(p3.name)  + " ," + str(p3.pais))
