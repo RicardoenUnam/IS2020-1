1587dc90ffc7579adb6d467fa779d7ea5f482765
Ricardo Ruiz <ori@ciencias.unam.mx>
2019-08-13 22:49:23 -0500

    main modified

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from alumno import *
 from materia import *
 from universidad import *
 
-p1 = Alumno(input("Escribe tu nombre: "), input("Escribe tu numero de cuenta: "))
+p1 = Alumno(input("Escribe tu nombre: "), input("Escribe tu # de cuenta: "))
 
 p2 = Materia(input("Escribe la materia que quieras registrar: "), input("Escribe el codigo de la materia: "))
 
